74bf6d35227f09d2b7801d22becd8975a69adde4
Andy Strube <andystrube64@gmail.com>
2024-02-27 14:10:53 -0800

    Hello World

--- main.cpp
+++ main.cpp
@@ -1,11 +1,6 @@
 #include <iostream>
-#include <vector>
 
 int main() {
-  std::vector nums {1,2,3,4,5};
-  for(auto num: nums) {
-    std::cout << num << std::endl;
-  }
-//   std::cout << "Hello world from vs code" << std::endl;
+  std::cout << "Hello World" << std::endl;
   return 0; 
 }
