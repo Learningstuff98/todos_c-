1a386e992cf109f59c5c3752a5c47daed768a9be
Andy Strube <andystrube64@gmail.com>
2024-02-27 13:49:51 -0800

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <vector>
+
+int main() {
+  std::vector nums {1,2,3,4,5};
+  for(auto num: nums) {
+    std::cout << num << std::endl;
+  }
+//   std::cout << "Hello world from vs code" << std::endl;
+  return 0;
+}
