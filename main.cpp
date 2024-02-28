42b3ccdf06873bde7336cc4cdbce3d0ca978fd68
Andy Strube <andystrube64@gmail.com>
2024-02-27 19:29:42 -0800

    Added validation for todo operation inputs

--- main.cpp
+++ main.cpp
@@ -1,33 +1,34 @@
 #include <iostream>
 #include <vector>
 
-void greet_user() {
+std::string get_desired_operation() {
   std::cout << "Please enter one of the following todo operations:" << std::endl;
   std::cout << "add, list, edit, delete or exit." << std::endl;
-};
-
-std::string get_desired_operation() {
-  greet_user();
   std::string desired_operation;
   std::cin >> desired_operation;
   return desired_operation;
 };
 
 std::string get_todo() {
-  std::cout << "Please enter a new todo" << std::endl;
+  std::cout << "Please enter a new todo:" << std::endl;
   std::string new_todo;
   std::cin >> new_todo;
   return new_todo;
-}
+};
 
 void list_todos(std::vector<std::string> const &todos) {
-  std::cout << "\n";
-  std::cout << "-------------------------" << std::endl;
+  std::cout << "\n-----------------------------" << std::endl;
   std::cout << "The current todos are as follows:" << std::endl;
   for(auto todo: todos) {
     std::cout << todo << std::endl;
   }
-  std::cout << "-------------------------\n\n";
+  std::cout << "-----------------------------\n\n";
+};
+
+void throw_error() {
+  std::cout << "\n-----------------------------" << std::endl;
+  std::cout << "ERROR! ERROR! INVALID INPUT!" << std::endl;
+  std::cout << "-----------------------------\n\n";
 };
 
 int main() {
@@ -37,10 +38,12 @@ int main() {
     if(desired_operation == "add") {
       todos.push_back(get_todo());
       desired_operation = get_desired_operation();
-    }
-    if(desired_operation == "list") {
-      list_todos(todos);
-      desired_operation = get_desired_operation();
+    } else if(desired_operation == "list") {
+        list_todos(todos);
+        desired_operation = get_desired_operation();
+    } else {
+        throw_error();
+        desired_operation = get_desired_operation();
     }
   }
   std::cout << "Program shutting down. Goodbye.";
